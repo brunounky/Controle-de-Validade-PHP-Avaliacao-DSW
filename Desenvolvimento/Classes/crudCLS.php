93e5a351f74d0cbc5ae97bcc2683e46a3aaf3785
brunounky <bruno_unky@hotmail.com>
2021-10-19 00:10:42 -0300

    Initial commit

--- Desenvolvimento/Classes/crudCLS.php
+++ Desenvolvimento/Classes/crudCLS.php
@@ -1,5 +1,20 @@
 <?php
 
 class crudCLS extends conexao{
-    
+
+private $crud;
+private $contador;
+
+private function preparedStatements($Query , $Parametros){
+
+$this->crud=$this->conexaoBD()->prepare($Query);
+
+}
+
+private function countParametros($Parametros){
+
+$this
+
+}
+
 }
